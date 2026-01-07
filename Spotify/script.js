41d04d42c130bff1e0badabeb8d61fd1b0b395f1
Rudra-cryptx <rudy.77goswami@gmail.com>
2026-01-07 10:19:43 +0530

    Made some new changes which wil make the website look more good

---
+++ Spotify/script.js
@@ -0,0 +1,34 @@
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:3000/Songs/")
+    let response = await a.text();
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let songs = [];
+    for (let i = 0; i < as.length; i++) {
+        const element = as[i];
+
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split(/Songs/)[1])
+        }
+    }
+    return songs;
+}
+
+async function main() {
+
+    let songs = await getSongs();
+    console.log(songs);
+
+    let songUl = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+
+    for(const song of songs){
+        songUl.innerHTML = songUl.innerHTML + `<li>${song.replaceAll("%5C", " ").replaceAll("%20", " ")}</li>`;
+    }
+
+    // var audio = new Audio(songs[0]);
+    // audio.play();
+
+}
+
+main();
